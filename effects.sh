0cdf484d48dd2af14ffee771f373b29d86119d6c
Thies Gehrmann <thiesgehrmann@gmail.com>
2013-11-13 15:44:43 +0100

    Updated effects

--- effects.sh
+++ effects.sh
@@ -3,28 +3,50 @@
 CLN_SEQ='complete_seq';
 EFCTS="fade overlay multiply reset";
 
-
+###############################################################################
 
 function effect_fade() {
   local tdir="$1";
   local n=`ls $dir | grep '^GOLS_3_[0-9]*.png$' | wc -l`;
+
+  i=100
+  for img in `ls $tdir | grep '^GOLS_3_[0-9]*.png$' | sort | tail -n10`; do
+    convert "$tdir/$img" -level $i% "$tdir/$img";
+    i=$((i-10));
+  done
+
   echo "Fade";
 }
+
+###############################################################################
 
 function effect_overlay() {
   local tdir="$1";
   local n=`ls $dir | grep '^GOLS_3_[0-9]*.png$' | wc -l`;
+
+  for img in `ls $tdir | grep '^GOLS_3_[0-9]*.png$'`; do
+    composite \( "$tdir/$ORIG" -resize 20% \)  -gravity SouthWest "$tdir/$img" "$tdir/$img";
+  done
   #composite \( GOLS_0_ORIG.png -resize 20% \) -gravity SouthWest GOLS_3_0000000000.png test.png
 
   echo "Overlay";
 }
 
+###############################################################################
+
 function effect_multiply() {
   local tdir="$1";
   local n=`ls $dir | grep '^GOLS_3_[0-9]*.png$' | wc -l`;
-  #composite GOLS_0_ORIG.png -compose Multiply SouthWest GOLS_3_0000000000.png test.png
+
+  for img in `ls $tdir | grep '^GOLS_3_[0-9]*.png$'`; do
+    composite "$tdir/$ORIG" -compose Multiply "$tdir/$img" "$tdir/$img";
+  done
+
+  #composite GOLS_0_ORIG.png -compose Multiply GOLS_3_0000000000.png test.png
   echo "Multiply";
 }
+
+###############################################################################
 
 function effect_reset() {
   local tdir="$1";
@@ -35,3 +57,6 @@ function effect_reset() {
   done
   echo "Reset";
 }
+
+###############################################################################
+
